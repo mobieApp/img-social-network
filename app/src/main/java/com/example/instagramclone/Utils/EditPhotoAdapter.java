c10791b93423247d4ad114dec8ce0a0660aebbc3
nmhieu0504 <84891428+nmhieu0504@users.noreply.github.com>
2022-03-21 16:07:39 +0700

    Edit Profile

--- app/src/main/java/com/example/instagramclone/Utils/EditPhotoAdapter.java
+++ app/src/main/java/com/example/instagramclone/Utils/EditPhotoAdapter.java
@@ -4,33 +4,35 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.instagramclone.R;
 
-public class EditPhotoAdapter extends RecyclerView.Adapter {
+public class EditPhotoAdapter extends RecyclerView.Adapter<EditPhotoAdapter.ViewHolder> {
 
     String[] items = {"Adjust", "Brightness", "Contrast", "Structure", "Warmth", "Saturation",
             "Color", "Fade", "Highlights", "Shadow", "Vignette", "Tilt Shift", "Sharpen"};
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         // Nạp layout cho View
         View editView = inflater.inflate(R.layout.single_edit_photo_item, parent, false);
 
-        RecyclerView.ViewHolder viewHolder = new ViewHolder(editView);
+        ViewHolder viewHolder = new ViewHolder(editView);
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textView.setText(items[position]);
     }
 
     @Override
@@ -40,10 +42,13 @@ public class EditPhotoAdapter extends RecyclerView.Adapter {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private View itemview;
+        TextView textView;
+        ImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             itemview = itemView;
+            textView = (TextView) itemView.findViewById(R.id.edit_name);
         }
     }
 }
